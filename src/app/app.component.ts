8b3afcaa0477553594df4211f9c7b713c92fe0b4
ChenMordoch <34474108+ChenMordoch@users.noreply.github.com>
2022-06-13 12:01:08 +0300

    create worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeCalculator } from './app.prime';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-worker-sample';
+  prime10 : number = 0;
+  prime10000 : number = 0;
+
+  find10thPrimeNumber() {
+      this.prime10 = PrimeCalculator.findNthPrimeNumber(10);
+  }
+
+  find10000thPrimeNumber() {
+    console.log('find10000thPrimeNumber');
+    // this.prime10000 = PrimeCalculator.findNthPrimeNumber(40000);
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      console.log('creating new web worker');
+      const worker = new Worker('./app.worker', { type: 'module' });
+      worker.onmessage = ({ data }) => {
+        console.log('data', data);
+        this.prime10000 = data;
+        worker.terminate();
+      };
+      worker.postMessage(10000);
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
 }
